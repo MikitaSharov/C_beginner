b740345148659501ffef6968c616bf343035038e
Nikita Sharov <GB@gmail.com>
2024-07-08 19:05:20 +0300

    hw1

---
+++ hw1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello, world!\n");
+
+    printf("\tLet’s\n");
+    printf("\t\tgo\n");
+    printf("\t\t\tto walk\n");
+
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+
+   printf(" __    __\n");
+   printf("/  \\__/  \\\n");
+   printf("\\__/  \\__/\n");
+   printf("/  \\__/  \\\n");
+   printf("\\__/  \\__/\n");
+
+    return 0;
+}
